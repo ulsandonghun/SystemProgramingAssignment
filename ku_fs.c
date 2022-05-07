21cf9463e2c7c8f33e5705bdb249de1cf9fb2870
donghun <donghoon141@naver.com>
2022-05-07 18:02:26 +0900

    현재 하나하나씩 정수값을 메세지로 보내는 것까지 구현함.

--- ku_fs.c
+++ ku_fs.c
@@ -4,8 +4,15 @@
 #include<signal.h>
 #include<stdlib.h>
 #include<string.h>
+#define MSG_SIZE sizeof(int)
+#define NAME "/m_queue"
+#define MAX_PRIO 5
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>        /* For mode constants */
+#include <mqueue.h>
 
 void generateChild();
+int searchFunc();
 int main(int argc,char *argv[]){
     printf("\n들어온 인자수 : %d\n",argc);
     char * str=argv[1];
@@ -13,8 +20,63 @@ int main(int argc,char *argv[]){
  
     printf("\n검색할 패턴 값: %s\n",str);
     printf("\n생성할 프로세스 수: %d\n",processNum);
-    pid_t pid[processNum];
-    generateChild(processNum,pid);
+    pid_t one;
+
+    if((one =fork())==0){
+        struct mq_attr attr;
+    int value;
+    unsigned int prio=0;
+    mqd_t mqdes;
+    
+
+    attr.mq_maxmsg=10;
+    attr.mq_msgsize=sizeof(value);
+
+    mqdes=mq_open(NAME, O_CREAT|O_RDWR,0666,&attr);
+    
+
+    /*for(int i=0;i<10;i++){
+        printf("sending a message %d\n",value);
+
+        mq_send(mqdes,(char*)&value,MSG_SIZE,prio);
+        value++;
+    }*/
+       
+         char *ptr=strstr(input,str);
+    int count=0;
+    while(ptr!=NULL){
+        int value=(ptr-input);
+        mq_send(mqdes,(char*)&value,sizeof(int),prio);
+        ptr=strstr(ptr+1,str);
+        count++;
+
+    }
+    mq_close(mqdes);
+    }
+    else{
+         struct mq_attr attr;
+    int value=0;
+  
+    int num_msg=0;
+    unsigned int prio;
+    mqd_t mqdes;
+
+    attr.mq_maxmsg=10;
+    attr.mq_msgsize=sizeof(value);
+
+    mqdes=mq_open(NAME,O_CREAT|O_RDWR,0666,&attr);
+
+    printf("\n받은 메세지 출력: ");
+   for(int i=0;i<10;i++){
+
+        mq_receive(mqdes,(char*)&value,MSG_SIZE,&prio);
+        printf("%d ",value);
+   }
+    
+       printf("\n");
+    }
+    //pid_t pid[processNum];
+    //generateChild(processNum,pid);
 
     for(int i=0;i<MAXS;i++){
         printf("%c ",input[i]);
@@ -34,12 +96,15 @@ int main(int argc,char *argv[]){
 }
 void generateChild(int num,pid_t pid[]){
     int childCount=0;
-    while(pid=fork){
+    while(pid[childCount]=fork()){
         childCount++;
         if(childCount==num){
             break;
         }
 
     }
+}
+int searchFunc(){
+    
 }
 
---
+++ ku_fs_input.h
@@ -0,0 +1,5 @@
+// This header file is a sample.
+
+#define MAXS 100
+// Input is defined as an arrangement of capital letters.
+char input[MAXS] = {'A','B','C','D','D','C','D','E','A','B','E','I','F','J','A','N','Q','L','F','G','A','B','B','A','D','P','G','N','D','K','G','Q','P','D','I','G','A','B','E','D','F','N','Q','N','A','B','E','I','G','H','F','N','A','B','E','K','F','N','S','L','E','K','N','K','F','O','Q','L','A','B','E','F','H','D','I','G','A','H','D','I','E','J','D','E','H','E','L','I','A','B','E','N','Q','S','B','N','S','L','D', 0};
